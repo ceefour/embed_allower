160f179357835bfb6eda26952ed972f674c19365
Hendy Irawan <hendy@rainbowpurple.com>
2008-10-20 12:26:38 +0700

    Including additions from Dan York:
    http://blogs.voxeo.com/behindtheblog/2008/08/01/tip-how-to-modify-wordpress-mu-to-allow-embed-objects-specifically-for-slideshare/

--- embed_allower.php
+++ embed_allower.php
@@ -1,12 +1,23 @@
 <?php
 /*
- * Plugin Name: Embed Allower
- * Plugin URI: http://hendyirawan.com/
- * Description: Allows embed, object, and other security risks tags. LGPLv3.
- * Version: 1.0
- * Author: Hendy Irawan
- * Author URI: http://hendyirawan.com/
- * */
+* Plugin Name: Embed Allower 2
+* Plugin URI: http://blogs.voxeo.com/behindtheblog/wp-plugin-embed-allower-2/
+* Description: Allows embed, object, and other security risks tags. LGPLv3.
+* Version: 1.0
+* Author: Dan York
+* Author URI: http://blogs.voxeo.com/behindtheblog/
+**/
+/*
+* Based on Embed Allower from Hendy Irawan at http://hendyirawan.com/
+*
+* WARNING: THERE ARE SERIOUS SECURITY RISKS with allowing these object and
+* embed tags, especially in multi-author environments where you don't trust
+* your users.  Please make sure you know what you are doing before using
+* this.
+*
+* Dan York and Voxeo Corporation assume absolutely no liability if you have any
+* security issues as a result of using this plugin. USE AT YOUR OWN RISK!
+*/
 
 // For more info see wp-includes/kses.php
 if (!CUSTOM_TAGS) {
@@ -27,6 +38,7 @@ if (!CUSTOM_TAGS) {
 		)
 	);
 	$allowedposttags['object'] = array(
+	        'style' => array (),
 		'height' => array (),
 		'width' => array (),
 		'param' => array (
@@ -39,7 +51,14 @@ if (!CUSTOM_TAGS) {
 			'id' => array (),
 			'height' => array (),
 			'width' => array (),
-			'src' => array ()
+			'src' => array (),
+			'allowfullscreen' => array (),
+			'allowscriptaccess' => array ()
 		)
 	);
+	$allowedposttags['param'] = array (
+	        'name' => array (),
+		'value' => array ()
+	);
 }
+?>
